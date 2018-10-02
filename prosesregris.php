21483d6306b584350c9925ae4927606734395bd3
faizazharr <43517978+faizazharr@users.noreply.github.com>
2018-10-02 16:53:47 +0700

    Update prosesregris.php

--- prosesregris.php
+++ prosesregris.php
@@ -1,4 +1,15 @@
-<?php
-
-
+<?php 
+include('connect.php');
+	session_start();
+	$nim = $_SESSION['nim'];
+	$nama = $_SESSION['nama'];
+	$email = $_SESSION['email'];
+	$sql = "INSERT INTO users(Nim,Nama,email) VALUES('$nim','$nama','$email')";
+			if(mysqli_query($conn,$sql)) {
+				echo "<center>SELAMAT ANDA TERDAFTAR</center>";
+			}else{
+				echo "Error: ".$sql."<br>".$conn->error;
+			}
+			$conn->close();
+	
 ?>
