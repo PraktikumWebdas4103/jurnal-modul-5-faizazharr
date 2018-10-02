4262d493926e24dd8a932057b482c3421bb98836
faizazharr <43517978+faizazharr@users.noreply.github.com>
2018-10-02 16:53:25 +0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -1,3 +1,18 @@
-<?php
+<form method="POST">
+	<textarea name="text"></textarea>
+	<input type="submit" name="simpan">
+</form>
 
+<?php  
+	if (isset($_POST['simpan'])) {
+		$text=$_POST['text'];
+		$c =str_word_count($text);
+		
+		if ($c < 5) {
+			echo "Komentar kurang dari 5 kata <br>";
+			echo "Jumlah komenter : ".$c;
+		}else{
+			echo "Jumlah komenter : ".$c;
+		}
+	}
 ?>
